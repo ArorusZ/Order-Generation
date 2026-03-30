7f2cbc45cfd174d200ca336157acf1266b0b48be
ArorusZ <82322782+ArorusZ@users.noreply.github.com>
2026-03-30 17:01:57 +0530

    Update ogw.R

--- project/dashboards/dashbords but more inside/ogw.R
+++ project/dashboards/dashbords but more inside/ogw.R
@@ -40,7 +40,7 @@ portfolioUI <- function(id) {
         br(),
         
         actionButton(ns("save"), "Save Data"),
-        column(4, br(), fileInput(ns("upload_excel"), "Upload from Excel", accept = ".xlsx")),
+        column(4, br(), fileInput(ns("upload_excel"), "Upload from Excel", accept = ".xlsx"),uiOutput(ns("sheet_selector"))),
         uiOutput(ns("sheet_selector")),
         
         br(), br(),
